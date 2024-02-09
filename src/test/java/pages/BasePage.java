5571b88804ce109774cbadfd1c9a7dcca6892c1e
RaduMihai <mrtwin999@yahoo.com>
2024-02-09 19:38:23 +0200

    Initial message commit

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pages.BasePage;
+
+public class BasePage {
+    private static final Logger LOG = LoggerFactory.getLogger(BasePage.class);
+    public static WebDriver driver;
+
+    public static void setUp() {
+        LOG.info("Start test");
+        System.setProperty("webdriver.chrome.driver", "C://WebDrivers/chromedriver121.exe");
+        driver = new ChromeDriver();
+        String url="https://demo.automationtesting.in/Index.html";
+        driver.get(url);
+
+        LOG.info("Open browser maximized");
+        driver.manage().window().maximize();
+    }
+
+    public static void close() {
+        LOG.info("Close the browser");
+        driver.quit();
+    }
+
+    public static String returnBaseUrl() {
+        return "https://demo.automationtesting.in/Index.html";
+    }
+
+    public static String getBaseUrl() {
+        String baseurl = returnBaseUrl();
+        if (baseurl != null) {
+            return baseurl.replace("Index.html", "");
+        }
+        return baseurl;
+    }
+}
